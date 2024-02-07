6fc62c02a215dabdb81556314450ee2b107b1b47
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-02-07 13:06:06 -0300

    dev header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-// Header component
 import React from 'react';
 import { Link } from 'react-router-dom'; // Assuming you're using react-router for navigation
 import WalletConnectButton from '../WalletConnectButton/WalletConnectButton'; // Adjust path as necessary
@@ -8,23 +7,40 @@ import styles from './header.css'; // This path assumes you are using CSS module
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>
-        <img src={logo} alt="NFT Marketplace Logo" className={styles.logo} />
+      <div className={styles.container}>
+        <Link
+          to="/"
+          aria-current="page"
+          className={styles.logoLink}
+        >
+          <img
+            src={logo}
+            alt="NFTKEY"
+            className={styles.logo}
+          />
+        </Link>
+        <div className={styles.buttonsContainer}>
+          <div className={styles.button}>
+            <Link
+              to="/price-history"
+              className={styles.link}
+            >
+              Price history
+            </Link>
+          </div>
+          <div className={styles.button}>
+            <Link
+              to="/collections"
+              className={styles.link}
+            >
+              Collections
+            </Link>
+          </div>
+          <div className={styles.darkModeButton}>
+            <WalletConnectButton />
+          </div>
+        </div>
       </div>
-      <nav className={styles.navbar}>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <Link to="/" className={styles.navLink}>Home</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link to="/marketplace" className={styles.navLink}>Marketplace</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link to="/about" className={styles.navLink}>About</Link>
-          </li>
-        </ul>
-      </nav>
-      <WalletConnectButton />
     </header>
   );
 };
